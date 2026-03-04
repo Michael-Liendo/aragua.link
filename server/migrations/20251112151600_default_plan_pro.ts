0bbb2c251c5bf67aa996bce4b59f50b1f1fc7521
Michael Liendo <michael.m.liendo.r@gmail.com>
2026-03-04 17:52:45 -0400

    refactor: replace old default user plan migration with updated version

---
+++ server/migrations/20251112151600_default_plan_pro.ts
@@ -0,0 +1,13 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.raw(
+		`ALTER TABLE "users" ALTER COLUMN "plan" SET DEFAULT 'PRO'`,
+	);
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.raw(
+		`ALTER TABLE "users" ALTER COLUMN "plan" SET DEFAULT 'FREE'`,
+	);
+}
